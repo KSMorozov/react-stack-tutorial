fe02c82581a55d340d0ebbe9a9eb01ed1dee0d69
Kirill Morozov <tiguhrrr@gmail.com>
2016-03-30 10:42:05 +0300

    Add chapter 5 code.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 const App = ({ subjects }) =>
   <ul>
     You like {subjects.map(subject => <li>{subject}</li>)}
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+  entry: path.join(__dirname, 'src', 'App.js'),
+  output: {
+    path: path.join(__dirname, 'src'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loader: 'babel',
+      include: path.join(__dirname, 'src')
+    }]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      }
+    })
+  ]
+};
